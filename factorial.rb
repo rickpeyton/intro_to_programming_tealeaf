288d5fb00943cc8b185d10fdabb3becc31d4e1a9
Rick Peyton <peytorb@gmail.com>
2014-12-28 13:17:57 -0600

    Finished basics exercises

---
+++ factorial.rb
@@ -0,0 +1,8 @@
+factorial = 8
+counter = 0
+total = 1
+while counter < factorial
+  counter += 1
+  total = (total * counter)
+end
+puts total
---
+++ movie_titles.rb
@@ -0,0 +1,13 @@
+movies = { diehard: 1988,
+           majorleague: 1989,
+           comingtoamerica: 1988 }
+
+puts movies[:diehard]
+puts movies[:majorleague]
+puts movies[:comingtoamerica]
+
+movies_array = [1988, 1989, 1988]
+
+puts movies_array[0]
+puts movies_array[1]
+puts movies_array[2]
